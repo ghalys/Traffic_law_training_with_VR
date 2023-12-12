26f8a5ef77e891c1d8ca170eca0014a933894a37
ghalys <asentissielidrissi@gmail.com>
2023-12-12 08:46:13 +0100

    canvas apparition du message du choc

--- Assets/Scripts/FinalLineDetection.cs
+++ Assets/Scripts/FinalLineDetection.cs
@@ -1,23 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FinalLineDetection : MonoBehaviour
 
 {
     // Définissez le message que vous souhaitez afficher
-    public string message = "Félicitations, vous avez atteint la ligne de fin !";
-
+    public GameObject canvas;
     private void OnTriggerEnter(Collider other)
     {
         // Vérifiez si l'objet qui entre en collision est le joueur (vous pouvez ajuster cela selon votre scénario)
+
         if (other.CompareTag("Vehicle"))
         {
-            // Affichez le message dans la console (vous pouvez remplacer cela par un affichage dans l'interface utilisateur)
-            Debug.Log(message);
+            canvas.SetActive(true);
+            TextMeshProUGUI texteDuCanvas = canvas.GetComponentInChildren<TextMeshProUGUI>();
 
-            // Vous pouvez également afficher le message dans l'interface utilisateur ou déclencher d'autres actions ici
-            // Exemple : UIManager.Instance.DisplayMessage(message);
+            if (texteDuCanvas != null)
+            {
+                texteDuCanvas.text = "Félicitations, vous avez atteint la ligne de fin !";
+            }
         }
     }
 }
